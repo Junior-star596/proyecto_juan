176ea9c162a653914e2363cda151befc0c6a70eb
Junior-star596 <juniorsarmiento742@gmail.com>
2025-03-05 20:13:44 -0400

    Commit inicial

---
+++ juego_pygame.py
@@ -0,0 +1,22 @@
+import pygame
+import random
+import math
+from enum import Enum
+import heapq
+
+# Inicialización de Pygame
+pygame.init()
+
+# Constantes
+ANCHO_PANTALLA = 800
+ALTO_PANTALLA = 600
+FPS = 60
+
+# Colores
+BLANCO = (255, 255, 255)
+NEGRO = (0, 0, 0)
+ROJO = (255, 0, 0)
+VERDE = (0, 255, 0)
+AZUL = (0, 0, 255)
+AMARILLO = (255, 255, 0)
+GRIS = (128, 128, 128)
